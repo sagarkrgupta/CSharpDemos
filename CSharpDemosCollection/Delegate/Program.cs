7be4187bbb5f60a8a6a702fefcb6e2e979306118
Sagar Gupta <gpt16sagar@hotmail.com>
2019-08-29 10:04:03 +0545

    basic delegate example done

---
+++ CSharpDemosCollection/Delegate/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        public delegate void addnum(int a, int b);
+        public delegate void subnum(int a, int b);
+
+
+        public delegate void rectDelegate(double height, double width);
+
+
+
+        static void Main(string[] args)
+        {
+
+            SomeClass d1 = new SomeClass();
+
+            //For ssingle delegate
+            // creating object of delegate, name as "del_obj1"  
+            addnum del_obj1 = new addnum(d1.AdditionFN);
+            subnum del_obj2 = new subnum(d1.SubtractionFN);
+
+            // pass the values to the methods by delegate object 
+            del_obj1(100, 40);
+            del_obj2(100, 60);
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+
+            //For ssingle delegate
+
+
+
+
+            // For MultiCasst Delegate
+
+            // creating delegate object, name as "rectdele" 
+            // and pass the method as parameter by  
+            // class object "rect" 
+            rectDelegate rectdele = new rectDelegate(d1.area);
+            //or
+            // rectDelegate rectdele1 = d1.area;
+
+            // call 2nd method "perimeter" 
+            // Multicasting 
+
+            rectdele += d1.perimeter;
+
+            // pass the values in two method  
+            // by using "Invoke" method 
+            rectdele.Invoke(6.3, 4.2);
+            Console.WriteLine();
+
+            // call the methods with
+            // different values 
+            rectdele.Invoke(16.3, 10.3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpDemosCollection/Delegate/SomeClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class SomeClass
+    {
+        
+        public void AdditionFN(int x,int y) =>  Console.WriteLine($"Addition of {x} and {y} is : {(x + y)}");
+        public void SubtractionFN(int x, int y) => Console.WriteLine($"Subtraction of {x} and {y} is : {(x - y)}");
+
+
+
+        // for multicast
+        // "area" method 
+        public void area(double height, double width) => Console.WriteLine("Area is: {0}", (width * height));
+        // "perimeter" method 
+        public void perimeter(double height, double width) => Console.WriteLine("Perimeter is: {0} ", 2 * (width + height));
+        
+
+
+    }
+}
